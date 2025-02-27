c6aafd85ffa7ea653e5fece06b90ec9334001647
tajfunik <hruska.marko11@gmail.com>
2025-02-27 14:57:13 +0100

    pridanie 'bcrypt' do projektu na kryptovanie hesla pri ukladani do DB, taktiez pri nacitani userov z databazy sa nezobrazi heslo, je to ochranny mechanizmus. Heslo si viem pozriet ked sa pozriem priamo do databazy cez cmd

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,12 @@
+import bcrypt from 'bcrypt';
 import User from '../models/users.js';
 
 
 export const getallUsers = async (req, res) =>{
     try {
         // Získanie všetkých Userov z databázy
-        const allUsers = await User.find();
+        const allUsers = await User.find({}, '-heslo'); // Vyber všetkých používateľov a vynechaj heslo
+        
         res.status(200).json(allUsers);
     } catch (error) {
         console.error(error);
@@ -14,9 +16,19 @@ export const getallUsers = async (req, res) =>{
 
 export const createUser = async (req, res) => {
     const { meno, heslo } = req.body;
+    const saltRounds = 10;  // Počet opakovaní hashovania
 
     try {
-        const newUser = new User({ meno, heslo });
+        // Skontrolovať, či meno a heslo sú zadané
+        if (!meno || !heslo) {
+            return res.status(400).json({ message: 'Vyplňte všetky polia.' });
+        }
+
+        // Hashovanie hesla
+        const hashedPassword = await bcrypt.hash(heslo, saltRounds);
+
+        // Vytvorenie nového používateľa s hashovaným heslom
+        const newUser = new User({ meno, heslo: hashedPassword });
         // Ulož užívateľa do databázy
         await newUser.save(); 
         res.status(201).json({ message: 'User bol úspešne pridaný', user: newUser });
--- server/models/users.js
+++ server/models/users.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 // Definovanie schémy pre usera
 const userSchema = new mongoose.Schema({
-    meno: { type: String, required: true },
+    meno: { type: String, required: true, unique: true },
     heslo: { type: String, required: true },
 });
 
 // Vytvorenie modelu z definovanej schémy
 //Mongoose automaticky vytvorí názov kolekcie v množnom čísle a v malých písmenách z názvu modelu
-//V tomto pripade nam v databaze vytvori z "Recept" = "users"
+//V tomto pripade nam v databaze vytvori z "User" = "users"
 const User = mongoose.model('user', userSchema);
 
 export default User;
