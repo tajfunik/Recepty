bd37d228d2e5bc0c835b5fe60f3cac8f6bc34b8e
tajfunik <hruska.marko11@gmail.com>
2026-07-21 15:24:00 +0200

    Pridanie subor na testovanie v Cypress

---
+++ cypress/e2e/interakcia.cy.ts
@@ -0,0 +1,3 @@
+it('vytvorenie karty', () => {
+    cy.visit('/')
+})
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,21 +1,84 @@
+// Hlavný blok popisujúci testy pre Login stránku
 describe('Login stránka', () => {
+
+  // beforeEach() sa spustí pred KAŽDÝM testom (it blokom)
+  // V tomto prípade zakaždým otvoríme login stránku
   beforeEach(() => {
-    cy.visit('http://localhost:3000/login');
+    cy.visit('/login');  // Otvorí stránku http://localhost:3000/login
   });
 
-  it('sa načíta správne a obsahuje potrebné prvky', () => {
+  // 1️⃣ Test: Načítanie stránky a kontrola všetkých prvkov
+  it('načíta stránku a zobrazí všetky prvky formulára', () => {
+
+    // Overíme, že sme na správnej URL adrese (obsahuje /login)
     cy.url().should('include', '/login');
 
-    cy.get('[data-cy=login-name]').should('exist');
-    cy.get('[data-cy=login-password]').should('exist');
-    cy.get('[data-cy=login-submit]').should('exist');
+    // Overíme, že stránka obsahuje nadpis "Prihlásenie"
+    cy.contains('h2', 'Prihlásenie').should('be.visible');
+
+    // Skontrolujeme, že input pre meno existuje a je prázdny
+    cy.get('[data-cy=login-name]')
+      .should('exist')           // input existuje v DOMe
+      .and('have.value', '');    // input je prázdny
+
+    // Skontrolujeme, že input pre heslo existuje a je prázdny
+    cy.get('[data-cy=login-password]')
+      .should('exist')
+      .and('have.value', '');
+
+    // Skontrolujeme, že tlačidlo na prihlásenie existuje a má správny text
+    cy.get('[data-cy=login-submit]')
+      .should('exist')
+      .and('contain.text', 'Prihlásiť sa');
+
+    // Overíme, že formulár (obalovací div) je viditeľný a na stránke je len jeden
+    cy.get('.form-container')
+      .should('be.visible')      // viditeľný pre používateľa
+      .and('have.length', 1);    // je tam len jeden takýto prvok
+  });
+  /*
+  // 2. Validacia alert okna
+  it('zobrazi chybu - alert okno, ak niektore z poli nie je vyplnene', () => {
+     // Zachytíme alert okno
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Zadajte prosím všetky údaje');
+    });
+
+    // Klikneme na tlačidlo bez vyplnenia polí
+    cy.get('[data-cy="login-submit"]').click();
   });
 
-  it('umožňuje vyplniť formulár a odoslať ho', () => {
-    cy.get('[data-cy=login-name]').type('testuser');
-    cy.get('[data-cy=login-password]').type('testpassword');
-    cy.get('[data-cy=login-submit]').click();
+  // 3. Vrati chybu pri nepsravnych prihlasovacich udajoch
+  it('zobrazí chybu pri nesprávnych prihlasovacích údajoch', () => {
+    cy.get('[data-cy="login-name"]').type('neexistujuci_uzivatel');
+    cy.get('[data-cy="login-password"]').type('zlenahoda');
 
-    // Tu môžeš pridať test na úspešný login (napr. presmerovanie alebo zobrazenie správy)
+    // Zachytíme alert z klienta
+    cy.on('window:alert', (text) => {
+      // Tu môžeš prispôsobiť podľa toho, čo reálne alertuješ
+      expect(text).to.contain('Chyba pri prihlásení');
+    });
+
+    cy.get('[data-cy="login-submit"]').click();
   });
+
+  //4. uspesne prihlasenie a presmerovanie
+  it('prihlási používateľa so správnymi údajmi a presmeruje ho', () => {
+    cy.get('[data-cy="login-name"]').type('Samko');        // <-- pridany skutocny user
+    cy.get('[data-cy="login-password"]').type('Samko1234'); // <-- pridaneskutocne heslo
+
+    cy.get('[data-cy="login-submit"]').click();
+
+    // Skontroluj, že používateľ bol presmerovaný na hlavnú stránku
+    cy.url().should('eq', 'http://localhost:3000/');
+
+    // Overenie uloženia údajov do localStorage
+    cy.window().then((win) => {
+      const meno = win.localStorage.getItem('loggedInUser');
+      const token = win.localStorage.getItem('token');
+
+      expect(meno).to.equal('Samko'); // konkretne meno, ktore sme pouzili vyssie 
+      expect(token).to.exist;
+    });
+  });*/
 });
